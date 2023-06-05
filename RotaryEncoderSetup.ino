56ac9e57b29613026bdeeaae297a2bbcdebcacf8
bjornahstrom <ahstrom_81@hotmail.com>
2023-06-05 18:49:16 +0200

    Added rotary function to react to only one state to avoid double count

--- RotaryEncoderSetup.ino
+++ RotaryEncoderSetup.ino
@@ -1,5 +1,5 @@
 void rotaryEncoderSetup() {
-  
+
   // Set rotary encoder pins as input
   pinMode(clk, INPUT);
   pinMode(dt, INPUT);
@@ -7,4 +7,26 @@ void rotaryEncoderSetup() {
 
   // Read the initial state of clk
   lastStateCLK = digitalRead(clk);
+}
+
+
+void checkRotaryEncoderStatus() {
+  // Read the current state of clk
+  currentStateCLK = digitalRead(clk);
+
+  // If last and current state of clkare different, then pulse ocurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK && currentStateCLK == 1) {
+
+    // If the dt state is different than the clk state then the encoder is rotating ccw so decrement
+    if (digitalRead(dt) != currentStateCLK) {
+      menuValue++;
+      Serial.println(menuValue);
+    } else {
+      menuValue--;
+      Serial.println(menuValue);
+    }
+    // Add Main menu function here
+  }
+  lastStateCLK = currentStateCLK;
 }
